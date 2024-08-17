f119a2f96bebf7d88797f79a69294f7c0bc74760
Ana Rad <cruelhamster25@gmail.com>
2024-08-17 10:44:07 +0300

    Add files via upload

---
+++ Peremennye.py
@@ -0,0 +1,10 @@
+# Объявляем переменные
+completed_tasks = 12
+spent_hours = 1.5
+course_name = 'Python'
+
+# Вычисляем время на одно задание
+average_time_per_task = spent_hours / completed_tasks
+
+# Выводим результат на экран
+print(f"Курс: {course_name}, всего задач: {completed_tasks}, затрачено часов: {spent_hours}, среднее время выполнения {average_time_per_task} часа.")
